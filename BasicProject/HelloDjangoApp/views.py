8f60c348cada3a32b1f2780ff171cb13f25273c2
Fernando Davis <ffdavis@outlook.com>
2018-08-08 11:34:36 -0400

    using templates

--- BasicProject/HelloDjangoApp/views.py
+++ BasicProject/HelloDjangoApp/views.py
@@ -1,8 +1,35 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+#def index(request):
+#    return HttpResponse("Hello, Django, Fer!")
+
+# from datetime import datetime
+
+# def index(request):
+#    now = datetime.now()
+
+#    html_content = "<html><head><title>Hello, Django</title></head><body>"
+#    html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+#    html_content += "</body></html>"
+
+#    return HttpResponse(html_content)
+
+from django.shortcuts import render   # Added for this step
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Hello, Django, Fer!")
+    now = datetime.now()
+
+    return render(
+        request,
+        "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
+        {
+            'title' : "Hello Django",
+            'message' : "Hello Django!",
+            'content' : " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
